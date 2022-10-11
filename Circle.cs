e9ab4cc871fa6e83c306a32f0ca4bbd1e2d0f990
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-10 21:11:07 -0400

    submitting circles

---
+++ Circle.cs
@@ -0,0 +1,57 @@
+﻿namespace L9Circles
+{
+    internal class Circle
+    {
+
+        //SETTING FIELD TO USE WHEN CALLING RADIUS
+        public double radius;
+
+        
+        //SETTING PROPERTY TO USE FIELD FOR RADIUS = RAD
+        public Circle(double rad)
+        {
+            radius = rad;
+
+        }
+
+
+        //METHOD FOR CALCULATING CIRCUMFERENCE OF CIRCLE
+        public double CalculateCircumference()
+        {
+            return 2 * (Math.PI * radius);
+        }
+
+        
+        //METHOD FOR FORMATTING CIRCUMFERENCE INTO A ROUNDED DECIMAL VALUE USING THE FORMAT METHOD IN CLASS CIRCLE
+        public string CalculateFormattedCircumference()
+        {
+            string formattedCircumference = FormatNumber(CalculateCircumference());
+            return formattedCircumference;
+        }
+
+
+        //METHOD FOR CALCULATING AREA OF CIRCLE
+        public double CalculateArea()
+        {   
+            double area = Math.PI * radius * radius;
+            return area;
+                
+        }
+
+
+        //METHOD FOR FORMATTING AREA INTO A ROUNDED DECIMAL VALUE USING THE FORMAT METHOD IN CLASS CIRCLE
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+
+
+        //METHOD FOR ROUDNING DECIMAL VALUES - WILL PASS TO OTHER METHODS
+        private string FormatNumber(double x)
+        {
+            
+            return Math.Round(x,2).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿namespace L9Circles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INITIALIZE VARIABLES
+            double radius = 0;
+            string area = "";
+            string circumference = "";
+            bool goAgain = true;
+            int circleCount = 0;
+
+
+            //GREETING  
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            //LOOP ASKING FOR INPUT ON RERUNNING THE PROGRAM
+            while (goAgain)
+            {
+                
+                //LOOP FOR VALIDATING USER INPUT
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter radius:");
+                        radius = double.Parse(Console.ReadLine());
+                        if (radius < 0)
+                        {
+                            Console.WriteLine("Please enter a positive number value");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+                }
+
+                //FUNCTION TO INCREMENT NUMBER OF CIRCLE RADIUS' REQUESTED
+                circleCount++;          
+
+
+                //CALLING CIRCLE CLASS TO INITIALIZE NEW VARIABLE
+                Circle myCircle = new Circle(radius);
+
+
+                //CALCULATION FOR AREA - USING CIRCLE CLASS
+                area = myCircle.CalculateFormattedArea();
+                //CALCULATION FOR CIRCUMFERENCE - USING CIRCLE CLASS
+                circumference = myCircle.CalculateFormattedCircumference();
+                //OUTPUT CALCULATIONS FOR AREA AND CIRCUMFERENCE TO CONSOLE
+                Console.WriteLine($"Circumference: {circumference}\nArea: {area}");
+
+ 
+                //LOOP TO INQUIRE IF THE USER WOULD LIKE TO CONTINUE - OTHERWISE OUTPUT CIRCLECOUNT
+                while (true)
+                {
+                    Console.WriteLine("Continue?(y/n)");
+                    string input = Console.ReadLine().ToLower().Trim();
+
+                    if (input == "n")
+                    {
+                        Console.WriteLine($"Goodbye. You created {circleCount} Circle object(s).");
+                        goAgain = false;
+                        break;
+                    }
+                    else if (input == "y")
+                    {
+                     
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter y/n");
+
+                    }
+                }
+            }
+        }
+    }
+}
